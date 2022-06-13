2fecc32f0f04935bd5b37aa872d53d8bdca0df74
Mathis Zerari <97899958+mathiszerari@users.noreply.github.com>
2022-06-13 14:53:33 +0200

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,12 @@
+<p><a href="index.html">Retour au Tableau de Bord</a></p>
+
+<?php
+  $title1 = htmlspecialchars($_GET['title1']);
+  $title2  = htmlspecialchars($_GET['title2']);
+  $title3  = htmlspecialchars($_GET['title3']);
+
+  echo htmlspecialchars($_GET['title1']);
+
+  print_r($_GET);
+
+  ?>
---
+++ website.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="styleweb.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CaminoTV</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="wrapper">
+            <div class="content">
+                <h1>yeezy foam runner</h1>
+                <h3>element3</h3>
+                <h2>là c'est le texte très long
+                </h2>
+            </div>
+        </div>
+    </header>
+
+</body>
+
+</html>
